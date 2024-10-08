1091c01a8391c92f4a5468fb72e603627320d5c4
rudyhuacasi <alexisitaliano16@gmail.com>
2024-10-08 17:56:48 +0200

    {UPD} added createpage and create form

---
+++ src/pages/CreatePage.vue
@@ -0,0 +1,21 @@
+<script>
+    export default{
+        name: 'CreatePage',
+    }
+</script>
+<template>
+    <div class="container">
+        <h1><em>Inserisci il medico</em></h1>
+        <div class="row row-gap-5">
+            <div class="col-4">
+                <router-link :to="{name:`createform`}">
+                    <div class="card p-5 d-flex align-items-center justify-content-center">
+                        +                    
+                    </div>
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+</style>
