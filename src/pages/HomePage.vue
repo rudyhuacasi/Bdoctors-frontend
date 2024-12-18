560dba8d5d9a19108343e4e210b364e94cf06baa
rudyhuacasi <alexisitaliano16@gmail.com>
2024-12-18 11:27:33 +0100

    {UPD} added home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -27,33 +27,31 @@
 </template>
 <style lang="scss" scoped>
 @use "../assets/scss/partials/variables" as *;
-    .fondo{
-        position: relative;
-        background-image: url('https://www.unidprofessional.com/wp-content/uploads/2023/04/Sanita-e-boom-di-medici-a-gettone-con-compensi-di-oro-1024x576.jpg');
-        background-repeat: no-repeat;
-        background-position: top;
-        background-size: cover;
-        width: 100%;
-        height: calc(100vh - 130px);
-    }
-    
-    .dancing-script{
-        z-index: 5;
-        font-family: "Caveat Variable";
-        font-optical-sizing: auto;
-        font-weight: 700;
-        color: #005dba;
-        text-shadow: 2px 2px 0px rgba(0, 0, 0, 3.5);
-        font-size: 55px;
+.fondo{
+    position: relative;
+    background-image: url('https://www.unidprofessional.com/wp-content/uploads/2023/04/Sanita-e-boom-di-medici-a-gettone-con-compensi-di-oro-1024x576.jpg');
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size: cover;
+    width: 100%;
+    height: calc(100vh - 130px);
+}
 
-    }
-    .position{
-        position: absolute;
-        top: 10%;
-        right: 5%;
-    }
+.dancing-script{
+    z-index: 5;
+    font-family: "Caveat Variable";
+    font-optical-sizing: auto;
+    font-weight: 700;
+    color: #005dba;
+    text-shadow: 2px 2px 0px rgba(0, 0, 0, 3.5);
+    font-size: 55px;
+}
+.position{
+    position: absolute;
+    top: 10%;
+    right: 5%;
+}
 
-    // button 
 .effect {
     color: $testo;
     padding: 13px 36px;
